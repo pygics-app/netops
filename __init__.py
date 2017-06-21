275b0e14371a867716370bf7a704582bd210d6ff
HyechurnJang <hyjang@cisco.com>
2017-06-21 18:16:04 +0900

    update

--- __init__.py
+++ __init__.py
@@ -263,9 +263,9 @@ def netops_main_total_table(table):
         if host.model == 'Unknown': model = ''
         else: model = host.model
         table.record(host.name,
-                     ANCH(HREF='http://' + dns, TARGET='_blank').html('http://' + dns),
+                     ANCH(HREF='http://' + dns, TARGET='_blank').html(dns),
                      '%s (%s)' % (host.range_name, host.range_type) if host.range_name != '' else host.range_name,
-                     ANCH(HREF='http://' + host.ip, TARGET='_blank').html('http://' + host.ip),
+                     ANCH(HREF='http://' + host.ip, TARGET='_blank').html(host.ip),
                      host.mac,
                      model,
                      host.serial,
